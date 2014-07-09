c7307a3964a76de1e23e44cf294ee1c28386aa79
cookiej4u <cookiej4u@gmail.com>
2014-07-09 18:10:05 +0800

    Exclude build files in platforms/

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Applications/MAMP/htdocs/roadKillCamera/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Applications/MAMP/htdocs/roadKillCamera/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Applications/MAMP/htdocs/roadKillCamera/platforms/android/ant-build/AndroidManifest.xml : \
-/Applications/MAMP/htdocs/roadKillCamera/platforms/android/AndroidManifest.xml \
