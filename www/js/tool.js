1a22a2a323c9be67f4fe39dc381ae096076540bd
cookiej4u <cookiej4u@gmail.com>
2014-07-10 19:17:30 +0800

    Add an article-posting test function. Connection switched to testbed.

--- www/js/tool.js
+++ www/js/tool.js
@@ -1,16 +1,17 @@
 
 function loginToDrupal(req, callback){
     console.log(req);
-    if(localStorage.getItem("SESS02294f16834559861ab86ca612e7e0a8")){
-        console.log(localStorage.getItem("SESS02294f16834559861ab86ca612e7e0a8"));
+    if(localStorage.getItem("sessName")){
+        console.log(localStorage.getItem("sessName"));
         callback("success");
         return;
     }
 
 
     // var dataUse = {username:'root', password:'drupalej03xu35k3'};
-    var dataUse = {username:'man27382210', password:'larry432'};
-    var url = "http://roadkill.tw/drupalgap/user/login";
+    //var dataUse = {username:'man27382210', password:'larry432'};
+    var dataUse = {username:'man27382210', password:'test'};
+    var url = "http://roadkill.tw/testbed/drupalgap/user/login";
     $.ajax({
         beforeSend: function (xhr){
                 // xhr.setRequestHeader("Authorization", authenticationRequestHeader);
@@ -21,8 +22,15 @@ function loginToDrupal(req, callback){
         url: url,
         data: dataUse,
         success: function(result){
-            localStorage.setItem("SESS02294f16834559861ab86ca612e7e0a8", result.sessid);
-            callback(result);
+            localStorage.setItem("sessName", result.session_name);
+            localStorage.setItem("sessId", result.sessid);
+            localStorage.setItem("token", result.token);
+            $.cookie(result.session_name, result.sessid);
+
+            console.log(localStorage.getItem("sessName"));
+            console.log(localStorage.getItem("sessId"));
+            console.log(localStorage.getItem("token"));
+            callback("success");
         },
         error: function(err){
             console.log(err);
@@ -32,12 +40,44 @@ function loginToDrupal(req, callback){
 
 function logoutToDrupal(callback){
     $.ajax({
-        url:"http://roadkill.tw/logout",
+        url:"http://roadkill.tw/testbed/drupalgap/user/logout",
+        type: 'POST',
+        dataType: 'json',
+        beforeSend: function (xhr){
+            xhr.setRequestHeader('X-CSRF-Token',
+                                localStorage.getItem("token"));
+        },
         success: function(result){
+            localStorage.removeItem("sessName");
+            localStorage.removeItem("sessId");
+            localStorage.removeItem("token");
             callback("success");
         },
         error:function(err){
             console.log(err);
         }
     });
+}
+
+function postToDrupal(callback) {
+    if(localStorage.getItem("sessName")) {
+        $.ajax({
+            url: "http://roadkill.tw/testbed/drupalgap/node",
+            type: 'POST',
+            dataType: 'json',
+            data: {"nid":"","title":"test","type":"article","language":"und","body":{"und":[{"value":"AJAX auto post!"}]},"field_image":{"und":[{"value":""}]},"field_placename":{"und":[{"value":""}]},"field_taxon_name":{"und":[{"value":""}]},"field_license_text":{"und":{"value":"1"}},"field_geo":{"und":[{"geom":{"lat":"43.465187","lon":"-80.522372"}}]}},
+            beforeSend: function (xhr){
+                xhr.setRequestHeader('X-CSRF-Token',
+                                    localStorage.getItem("token"));
+            },
+            success: function(result){
+                callback("success");
+            },
+            error: function(err){
+                console.log(err);
+            }
+        });
+    } else {
+        callback("Please log in first.");
+    }
 }
